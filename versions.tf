daf7bb3bb1abd5d0303af95b9fe970e19deefc46
Jose Angel Munoz <5019972+imjoseangel@users.noreply.github.com>
2021-06-22 19:51:16 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.61.0"
+      version = "~> 2.64.0"
     }
   }
-  required_version = ">= 0.15"
+  required_version = ">= 1.0"
 }
